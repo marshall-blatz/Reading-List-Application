d64fa545fc72beeddb3c0c8374e3e75a37ed00f2
Marshall Blatz <marshall-blatz@uiowa.edu>
2021-02-23 00:16:18 -0500

    Update app.js
    Set the plan for the file in comments

---
+++ app.js
@@ -0,0 +1,11 @@
+// Book Class: Represents a book
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles Storage Locally
+
+// Event: Display Books
+
+// Event: Add a Book
+
+// Event: Remove a Book
